4e30a9fda41a7f5473ba0655397e2260424648f0
Vlad Dorokh <vlad.doroh@gmail.com>
2022-01-20 14:13:03 +0300

    Added all internship projects.

---
+++ MongoDBTestApp/MongoDBTestApp/Controllers/BooksController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDBTestApp.Models;
+using MongoDBTestApp.Services;
+
+namespace MongoDBTestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        // GET: api/Book
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> Get()
+        {
+            return await _bookService.Get();
+        }
+
+        // GET: api/Book/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            var s = await _bookService.Get(id);
+            if (s == null)
+            {
+                return NotFound();
+            }
+
+            return s;
+        }
+
+        // POST: api/Book
+        [HttpPost]
+        public async Task<ActionResult<Book>> Create([FromBody] Book s)
+        {
+            await _bookService.Create(s);
+            return CreatedAtRoute("Get", new { id = s.Id.ToString() }, s);
+
+        }
+
+        // PUT: api/Book/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Book>> Put(string id, [FromBody] Book su)
+        {
+            var s = await _bookService.Get(id);
+            if (s == null)
+            {
+                return NotFound();
+            }
+            su.Id = s.Id;
+
+            await _bookService.Update(id, su);
+            return CreatedAtRoute("Get", new { id = su.Id.ToString() }, su);
+        }
+
+        // DELETE: api/Book/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Book>> Delete(string id)
+        {
+            var s = await _bookService.Get(id);
+            if (s == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ MongoDBTestApp/MongoDBTestApp/Services/BookService.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Driver;
+using MongoDBTestApp.Models;
+
+namespace MongoDBTestApp.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _book;
+
+        public BookService(IConfiguration config)
+        {
+            // Connects to MongoDB.
+            var client = new MongoClient(config.GetConnectionString("BookDB"));
+            // Gets the BookDB.
+            var database = client.GetDatabase("BookStore");
+            //Fetches the Book collection.
+            _book = database.GetCollection<Book>("Books");
+        }
+
+        public async Task<List<Book>> Get()
+        {
+            //Gets all Books. 
+            return await _book.Find(s => true).ToListAsync();
+        }
+
+        public async Task<Book> Get(string id)
+        {
+            //Get a single Book. 
+            return await _book.Find(s => s.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Book> Create(Book s)
+        {
+            //Create a Book.
+            await _book.InsertOneAsync(s);
+            return s;
+        }
+
+        public async Task<Book> Update(string id, Book s)
+        {
+            // Updates and existing Book. 
+            await _book.ReplaceOneAsync(su => su.Id == id, s);
+            return s;
+        }
+
+
+        public async Task Remove(string id)
+        {
+            //Removes a Book.
+            await _book.DeleteOneAsync(su => su.Id == id);
+        }
+
+    }
+}
